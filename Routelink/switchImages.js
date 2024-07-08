1d976b2507853556ea670b450a0b878bf65a5a4e
AlphaPaulCode <erioroegbelughepaul@gmail.com>
2024-07-08 10:26:42 +0100

    almost done

--- Routelink/switchImages.js
+++ Routelink/switchImages.js
@@ -1,4 +1,9 @@
 
+function toggleMenu() {
+    const navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('mobile-active');
+}
+
         document.addEventListener('DOMContentLoaded', function() {
             const images = document.querySelectorAll('.background-image img');
             let currentImageIndex = 0;
